cb93a29546138bf1384e1b3690e8f59d8e168cdc
kelvin <kelvinadamba@hotmail.com>
2021-10-04 21:46:15 +0300

    introduced variable blocks in index.htmla

--- app/views.py
+++ app/views.py
@@ -7,4 +7,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello World This is an upcoming developer '
+    return render_template('index.html',message = message)
