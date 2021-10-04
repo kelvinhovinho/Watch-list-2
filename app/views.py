616cd8d7665699de519603e80fd19bc09f78c4ad
kelvin <kelvinadamba@hotmail.com>
2021-10-05 00:16:20 +0300

    used control flows to add a title in index.html

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome to The Best Movie Review Website Online'
     message = 'Hello World This is an upcoming developer '
-    return render_template('index.html',message = message)
+    return render_template('index.html',title = title,message = message)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
